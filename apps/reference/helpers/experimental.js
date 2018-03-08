6fdb48992ecb446d9bcf032c088e4230b90fb06b
Tanguy Crusson <tcrusson@atlassian.com>
2018-03-08 10:42:29 +0100

    wrapped up example for message inline action

--- apps/reference/helpers/experimental.js
+++ apps/reference/helpers/experimental.js
@@ -3,113 +3,88 @@ const { Document } = require("adf-builder");
 const app_name = process.env.APP_NAME || "Stride Reference App";
 
 module.exports.messageWithInlineActions = () => {
-	return {
-		"version": 1,
-		"type": "doc",
-		"content": [
-			{
-				"type": "paragraph",
-				"content": [
-					{
-						"type": "text",
-						"text": "Do you approve?"
-					}
-				]
-			},
-			{
-				"type": "bodiedExtension",
-				"attrs": {
-					"extensionType": "com.atlassian.stride",
-					"extensionKey": "actionGroup"
-				},
-				"content": [
-					{
-						"type": "paragraph",
-						"content": [
-							{
-								"type": "inlineExtension",
-								"attrs": {
-									"extensionType": "com.atlassian.stride",
-									"extensionKey": "action",
-									"parameters": {
-										"title": "Approve",
-										"appearance":"default",
-										"action": {
-											"target": {
-												"key": "actionTarget-handleInlineMessageAction"
-											},
-											"parameters": {
-												"reason": "approve"
-											}
-										}
-									}
-								}
-							},
-							{
-								"type": "inlineExtension",
-								"attrs": {
-									"extensionType": "com.atlassian.stride",
-									"extensionKey": "action",
-									"parameters": {
-										"title": "Reject",
-										"appearance":"default",
-										"action": {
-											"target": {
-												"key": "actionTarget-handleInlineMessageAction"
-											},
-											"parameters": {
-												"reason": "reject"
-											}
-										}
-									}
-								}
-							}
-						]
-					}
-				]
-			}
-		]
-	}
+  return {
+    "version": 1,
+    "type": "doc",
+    "content": [
+      {
+        "type": "paragraph",
+        "content": [
+          {
+            "type": "text",
+            "text": "Do you approve?"
+          }
+        ]
+      },
+      {
+        "type": "bodiedExtension",
+        "attrs": {
+          "extensionType": "com.atlassian.stride",
+          "extensionKey": "actionGroup"
+        },
+        "content": [
+          {
+            "type": "paragraph",
+            "content": [
+              {
+                "type": "inlineExtension",
+                "attrs": {
+                  "extensionType": "com.atlassian.stride",
+                  "extensionKey": "action",
+                  "parameters": {
+                    "title": "Approve",
+                    "appearance": "default",
+                    "action": {
+                      "target": {
+                        "key": "actionTarget-handleInlineMessageAction"
+                      },
+                      "parameters": {
+                        "reason": "approve"
+                      }
+                    }
+                  }
+                }
+              },
+              {
+                "type": "inlineExtension",
+                "attrs": {
+                  "extensionType": "com.atlassian.stride",
+                  "extensionKey": "action",
+                  "parameters": {
+                    "title": "Reject",
+                    "appearance": "default",
+                    "action": {
+                      "target": {
+                        "key": "actionTarget-handleInlineMessageAction"
+                      },
+                      "parameters": {
+                        "reason": "reject"
+                      }
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports.inlineActionResponseMessage = (userId, reason) => {
-	let approvalTxt =
-			reason === "approve" ?
-					"Approved by ": "Rejected by ";
+  let approvalTxt =
+    reason === "approve" ?
+      "Approved by " : "Rejected by ";
 
-	return {
-		"version": 1,
-		"type": "doc",
-		"content": [
-			{
-				"type": "paragraph",
-				"content": [
-					{
-						"type": "text",
-						"text": "Do you approve?"
-					}
-				]
-			},
-			{
-				"type": "paragraph",
-				"content": [
-					{
-						"type": "text",
-						"text": approvalTxt,
-						"marks": [
-							{
-								"type": "strong"
-							}
-						]
-					},
-					{
-						"type": "mention",
-						"attrs": {
-							"id": userId
-						}
-					}
-				]
-			}
-		]
-	}
+  const doc = new Document();
+  doc
+    .paragraph()
+    .text("Do you approve?");
+  doc.paragraph()
+    .strong(approvalTxt)
+    .mention(userId)
+  doc.paragraph()
+    .em("Note: the contract of this node will change soon. Also, the target solution supports loading/error states")
+  return doc.toJSON();
 }
