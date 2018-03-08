37d943d5da830e3dddd40975888376b357c64312
Pratik Shah <pshah@atlassian.com>
2018-03-08 08:42:11 -0600

    cleaned up how packages are handled.

--- install.sh
+++ install.sh
@@ -6,29 +6,30 @@ CBLUE=$(printf '\033[0;34m')
 CGRAY=$(printf '\033[1;30m')
 CRED=$(printf '\033[0;31m')
 CPURPLE=$(printf '\033[0;35m')
+reset=$(printf '\033[0;m')
 
 JS_CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/src" && pwd )"
 REFAPP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/apps/reference" && pwd )"
-SIMPLE_APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/apps/hello" && pwd )"
-echo "$REFAPP_DIR"
+HELLO_APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/apps/hello" && pwd )"
 
 
 client_install() {
  echo "${CCYAN}Installing stride NodeJS client packages.."
- cd "$JS_CLIENT_DIR" && npm i
+ (cd "$JS_CLIENT_DIR" && npm install)
  echo "Done with installation for stride packages"
 
 }
 
 reference_install() {
 echo "${CBLUE} Installing reference application packages.."
-cd "$REFAPP_DIR" && npm i
+ (cd "$REFAPP_DIR" && npm install)
  echo "Done with installation for reference app packages"
 }
 
 hello_install() {
 echo "${CGREEN}Installing hello application packages.."
-cd "$SIMPLE_APP_DIR" && npm i
+ (cd "$HELLO_APP_DIR" && npm install)
+
  echo "Done with installation for hello app packages"
 }
 
@@ -43,7 +44,7 @@ hello_install
 
 printf "\n"
 echo "${CPURPLE}<=====Successfully completed installation=======>"
-
+echo ${reset}
 }
 
 main
