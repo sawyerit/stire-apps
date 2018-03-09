65057d4994cb8c6d7dc2bfafe7e356abcb05c7e4
Tanguy Crusson <tcrusson@atlassian.com>
2018-03-09 11:26:28 +0100

    removed 2 showcase items because of bugs. Updated the configuration examples

---
+++ apps/reference/public/js/configuration.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+
+  /**
+   * Load configuration settings from the app backend for this conversation
+   */
+  AP.auth.withToken(function (err, token) {
+    $.ajax(
+      {
+        type: 'GET',
+        url: '/module/config/content',
+        headers: {'Authorization': 'Bearer ' + token},
+        dataType: 'json',
+        success: function (data) {
+          var notificationLevel = data.notificationLevel;
+          switch (notificationLevel) {
+            case "INSTANT":
+              $("#option1").prop("checked", true);
+              break;
+            case "DAILY":
+              $("#option2").prop("checked", true);
+              break;
+            default:
+              $("#option3").prop("checked", true);
+          }
+        },
+        error: function (data) {
+          console.log(data);
+        }
+      });
+  });
+
+  /**
+   * Call the app backend to save configuration settings when the user clicks on the "Save" action
+   */
+  AP.register({
+    "dialogAction-saveConfiguration": function () {
+
+      AP.dialog.disableActions();
+
+      var notificationLevel = $("input[id=option1]:checked").val() ? "INSTANT" :
+        $("input[id=option2]:checked").val() ? "DAILY" : "NONE";
+
+      AP.auth.withToken(function (err, token) {
+        $.ajax(
+          {
+            type: 'POST',
+            url: '/module/config/content',
+            headers: {'Authorization': 'Bearer ' + token},
+            data: {'notificationLevel': notificationLevel},
+            dataType: 'json',
+            success: function (data) {
+              AP.action.openTarget({
+                "target": {
+                  "key": "actionTarget-openSidebar-showcase"
+                }
+              });
+              AP.dialog.close();
+            },
+            error: function (data) {
+              console.log("Error" + data);
+            }
+          });
+      });
+
+    },
+    "dialogAction-closeConfiguration": function () {
+      AP.action.openTarget({
+        "target": {
+          "key": "actionTarget-openSidebar-showcase"
+        }
+      });
+      AP.dialog.close();
+    }
+  });
+});
